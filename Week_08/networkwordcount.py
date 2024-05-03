492d93c4b957a64c45cfc2acd0a3ed378a772580
jensbaetens <jens.baetens@ikdoeinformatica.be>
2024-05-03 15:34:39 +0200

    voorbeeld 2

--- Week_08/networkwordcount.py
+++ Week_08/networkwordcount.py
@@ -1,11 +1,15 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+from datetime import datetime
 
-# Create a local StreamingContext with two working thread and batch interval of 5 second
+# Create a local StreamingContext with two working thread and batch interval of 1 second
 sc = SparkContext("local[2]", "networkwordcount")
 sc.setLogLevel("ERROR") # reduce spam of logging
+
 # zet de sparkcontext om naar een streaming context dat om de 5 seconden uitgevoerd wordt
 ssc = StreamingContext(sc, 5)
+ssc.checkpoint("checkpoint")
+
 # lees data van de localhost-poort 19999
 lines = ssc.socketTextStream('localhost',19999)
 
@@ -16,7 +20,21 @@ words = words.map(lambda word: (word,1))
 # reduce van mapreduce (tel de 1-tjes op per woord)
 counts = words.reduceByKey(lambda x,y: x+y)
 
-counts.pprint()
+# Print the running wordcount / global state
+def updateFunction(newValues, runningCount):
+    if runningCount is None:
+        runningCount = 0
+    return sum(newValues, runningCount)
+
+#def updateFunction(new_values, last_sum):
+#    return sum(new_values) + (last_sum or 0)
+
+def updateFunction(new_values, timestamps):
+    return datetime.now()
+
+state = counts.updateStateByKey(updateFunction)
+
+state.pprint()
 
 # start de berekeningen
 ssc.start()
